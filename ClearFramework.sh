9b420df8a16b31177a9fda2cdfd475c67c40e874
Hendrik Roch <hroch@physik.uni-bielefeld.de>
2022-09-27 11:54:24 +0200

    clears all framework files

---
+++ ClearFramework.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+rm -rf KoMPoST
+rm -rf MUSIC
+rm -rf iSS
+rm -rf smash
+rm pythia8307.tgz
+rm eigen-3.4.0.tar.gz
+rm -rf pythia8307
+rm -rf eigen-3.4.0
+
+echo "Cleaned all the modules successfully"
